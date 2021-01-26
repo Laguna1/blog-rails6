2806a6e7970be3d2bfb2e1e07d3c34fbfe922024
Laguna1 <lagunazoomarket@gmail.com>
2021-01-26 20:17:52 +0200

    Add partial article form and navigation links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,15 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def index
+        @articles = Article.all
+      end
+
     def show
+        @article = Article.find(params[:id])
+       end
+
+       def edit
         @article = Article.find(params[:id])
        end
 
@@ -15,6 +23,24 @@ class ArticlesController < ApplicationController
         else
          render 'new'
         end
+       end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+         flash[:notice] = "Article was updated"
+         redirect_to article_path(@article)
+        else
+         flash[:notice] = "Article was not updated"
+         render 'edit'
+        end
+      end
+
+      def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was deleted"
+        redirect_to articles_path
        end
 
       private
