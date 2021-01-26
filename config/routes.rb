453b468847b42df8f5844177085cc39571f260ca
Laguna1 <lagunazoomarket@gmail.com>
2021-01-26 02:02:09 +0200

    Add Article model, validations, routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
+
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
